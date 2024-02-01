784d2aa65614e83f218ad0dc3045f9db95ad32f3
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-02-01 10:17:52 +0100

    add user and Role entities + configure connection with database

---
+++ src/main/java/com/example/s3cur1ty/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.s3cur1ty.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+
+
+}
---
+++ src/main/java/com/example/s3cur1ty/domain/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.example.s3cur1ty.domain.enums;
+
+public enum UserRole {
+    USER,
+    ADMIN,
+    SUPER_ADMIN
+
+}
